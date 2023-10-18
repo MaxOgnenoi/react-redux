fd294118ea2a7c3a595e14accd82d7b46d109081
Max Ognenoi <maksymohnenyi>
2023-10-17 23:27:58 -0400

    new functionality

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { CREATE_POST, FETCH_POST } from "./types";
+import { CREATE_POST, FETCH_POSTS, HIDE_ALERT, HIDE_LOADER, SHOW_ALERT, SHOW_LOADER } from "./types";
 
 export function createPost() {
     return {
@@ -7,10 +7,52 @@ export function createPost() {
     }
 }
 
+export function showLoader() {
+    return {
+        type: SHOW_LOADER
+    }
+}
+
+export function hideLoader() {
+    return {
+        type: HIDE_LOADER
+    }
+}
+
+export function showAlert(text) {
+    return dispatch => {
+        dispatch({
+            type: SHOW_ALERT,
+            payload: text
+        })
+
+        setTimeout(() => {
+            dispatch(hideAlert())
+        }, 3000)
+    }
+}
+
+export function hideAlert() {
+    return {
+        type: HIDE_ALERT
+    }
+}
+
 export function fetchPosts() {
     return async dispatch => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-        const json = await response.json()
-        dispatch({ type: FETCH_POST, payload: json })
+        try {
+            dispatch(showLoader())
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+            const json = await response.json()
+            setTimeout(() => {
+                dispatch({ type: FETCH_POSTS, payload: json })
+                dispatch(hideLoader())
+            }, 500)
+        } catch (error) {
+            dispatch(showAlert('Something goes wrong'))
+            dispatch(hideLoader())
+        }
+
+
     }
 }
